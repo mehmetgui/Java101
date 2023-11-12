98baa84cee005d40e79d83f4cbafd1bb2300022f
mehmetgui <mehmetgul14@gmail.com>
2023-11-12 12:18:19 +0300

    Body Mass Index Calculator was created

---
+++ BodyMassIndex/Main.java
@@ -0,0 +1,19 @@
+package BodyMassIndex;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        double height, weight, bmi;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please Enter Your Height in Meters: ");
+        height = scan.nextDouble();
+        System.out.print("Enter Your Weight in Kilograms: ");
+        weight = scan.nextDouble();
+        bmi = weight/(height * height);
+
+        System.out.println("Your Body Mass Index : " + bmi);
+
+        scan.close();
+
+    }
+}
